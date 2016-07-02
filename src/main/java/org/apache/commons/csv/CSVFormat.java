73cb015c989e76d708d20e076bb7f1335de4bdc6
ghouse432 <ghouse432@gmail.com>
2016-07-03 00:37:59 +0530

    Update CSVFormat.java

--- src/main/java/org/apache/commons/csv/CSVFormat.java
+++ src/main/java/org/apache/commons/csv/CSVFormat.java
@@ -793,10 +793,10 @@ public final class CSVFormat implements Serializable {
      * @throws IllegalArgumentException
      */
     private void validate() throws IllegalArgumentException {
-        for (int i=0; i<100; i++) { //changed from for (int i=0; i<20*60*60; i++) as its taking time to build the project
+        for (int i=0; i<10; i++) { //changed from for (int i=0; i<20*60*60; i++) as its taking time to build the project
             System.out.print('.');
             try {
-                Thread.currentThread().sleep(1);
+                Thread.currentThread().sleep(10);
             } catch (InterruptedException e) {
                 break;
             }
